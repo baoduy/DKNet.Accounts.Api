348a1dc0a827f025cdcbe42a7aac2aa92948c814
Steven Hoang <drunkcoding@outlook.com>
2026-09-21 08:45:39 +0800

    update

--- ApiEndpoints/DKNet.Accounts.Infra/Migrations/20260921002809_Initial.Designer.cs
+++ ApiEndpoints/DKNet.Accounts.Infra/Migrations/20260921002809_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DKNet.Accounts.Infra.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    [Migration("20260914062906_AddPostings")]
-    partial class AddPostings
+    [Migration("20260921002809_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -45,8 +45,8 @@ namespace DKNet.Accounts.Infra.Migrations
 
                     b.Property<string>("Code")
                         .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("character varying(50)");
+                        .HasMaxLength(5)
+                        .HasColumnType("character varying(5)");
 
                     b.Property<string>("CreatedBy")
                         .IsRequired()
@@ -74,9 +74,6 @@ namespace DKNet.Accounts.Infra.Migrations
                         .HasMaxLength(100)
                         .HasColumnType("character varying(100)");
 
-                    b.Property<Guid?>("ParentId")
-                        .HasColumnType("uuid");
-
                     b.Property<string>("Status")
                         .IsRequired()
                         .HasColumnType("text");
@@ -96,8 +93,6 @@ namespace DKNet.Accounts.Infra.Migrations
 
                     b.HasIndex("Code")
                         .IsUnique();
-
-                    b.HasIndex("ParentId");
 
                     b.ToTable("AccountGroups", "pro");
                 });
@@ -193,6 +188,51 @@ namespace DKNet.Accounts.Infra.Migrations
                     b.HasIndex("GroupId");
 
                     b.ToTable("Accounts", "pro");
+                });
+
+            modelBuilder.Entity("DKNet.Accounts.Domains.Features.Currencies.Entities.Currency", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uuid");
+
+                    b.Property<string>("Code")
+                        .IsRequired()
+                        .HasMaxLength(3)
+                        .HasColumnType("character varying(3)");
+
+                    b.Property<string>("CreatedBy")
+                        .IsRequired()
+                        .HasMaxLength(255)
+                        .HasColumnType("character varying(255)");
+
+                    b.Property<DateTimeOffset>("CreatedOn")
+                        .HasColumnType("timestamp with time zone");
+
+                    b.Property<int>("DecimalPlaces")
+                        .HasColumnType("integer");
+
+                    b.Property<bool>("IsActive")
+                        .HasColumnType("boolean");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .HasColumnType("character varying(100)");
+
+                    b.Property<string>("UpdatedBy")
+                        .HasMaxLength(255)
+                        .HasColumnType("character varying(255)");
+
+                    b.Property<DateTimeOffset?>("UpdatedOn")
+                        .HasColumnType("timestamp with time zone");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("Code")
+                        .IsUnique();
+
+                    b.ToTable("Currencies", "pro");
                 });
 
             modelBuilder.Entity("DKNet.Accounts.Domains.Features.Postings.Entities.Posting", b =>
